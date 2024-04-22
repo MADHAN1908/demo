b3da31c59bb56c920bef21cc7bd375e50cf71249
madhan <162275786+MADHAN1908@users.noreply.github.com>
2024-04-22 14:06:09 +0530

    1

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask,g,render_template,send_file,request, redirect, url_for,session
-import subprocess
+from flask import Flask,g,render_template,send_file,request, redirect, url_for
 import base64,os
 from io import BytesIO
 from datetime import datetime, timedelta
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
+
 import sqlite3
 from db import *
 app = Flask(__name__)
@@ -24,43 +21,43 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-def getlog(l_id):
-    db= get_db()
-    cur = db.cursor()
-    cur.execute('select * from user_log where log_id = ? ',(l_id,))
-    log=cur.fetchone()
-    print(log)
-    # cur.close()
-    return log
+# def getlog(l_id):
+#     db= get_db()
+#     cur = db.cursor()
+#     cur.execute('select * from user_log where log_id = ? ',(l_id,))
+#     log=cur.fetchone()
+#     print(log)
+#     # cur.close()
+#     return log
 
-def get_next_module_no(course_id):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute('''
-        SELECT MAX(module_no) FROM course_module WHERE course_id = ?;
-    ''', (course_id,))
-    max_module_no = cursor.fetchone()[0]
-    return (max_module_no + 1) if max_module_no is not None else 1
+# def get_next_module_no(course_id):
+#     db = get_db()
+#     cursor = db.cursor()
+#     cursor.execute('''
+#         SELECT MAX(module_no) FROM course_module WHERE course_id = ?;
+#     ''', (course_id,))
+#     max_module_no = cursor.fetchone()[0]
+#     return (max_module_no + 1) if max_module_no is not None else 1
 
-def get_next_question_no(course_id):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute('SELECT MAX(question_no) FROM quiz WHERE course_id = ?;', (course_id,))
-    max_question_no = cursor.fetchone()[0]
-    return (max_question_no + 1) if max_question_no is not None else 1
+# def get_next_question_no(course_id):
+#     db = get_db()
+#     cursor = db.cursor()
+#     cursor.execute('SELECT MAX(question_no) FROM quiz WHERE course_id = ?;', (course_id,))
+#     max_question_no = cursor.fetchone()[0]
+#     return (max_question_no + 1) if max_question_no is not None else 1
 
-def get_next_module_question_no(course_id,module_no):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute('SELECT MAX(question_no) FROM module_quiz WHERE course_id = ? and module_no= ?;', (course_id,module_no))
-    max_question_no = cursor.fetchone()[0]
-    return (max_question_no + 1) if max_question_no is not None else 1
+# def get_next_module_question_no(course_id,module_no):
+#     db = get_db()
+#     cursor = db.cursor()
+#     cursor.execute('SELECT MAX(question_no) FROM module_quiz WHERE course_id = ? and module_no= ?;', (course_id,module_no))
+#     max_question_no = cursor.fetchone()[0]
+#     return (max_question_no + 1) if max_question_no is not None else 1
   
 @app.route('/')
 def home():
     create_table()
     add_module_count_trigger()
-    return "Welcome"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
