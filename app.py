623abdbefe4ed023009ffbc2352cf62a9cb0f7da
madhan <162275786+MADHAN1908@users.noreply.github.com>
2024-04-22 14:38:00 +0530

    ds

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask,g,render_template,send_file,request, redirect, url_for,session
-import subprocess
-import base64,os
-from io import BytesIO
-from datetime import datetime, timedelta
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
+
 from db import *
 app = Flask(__name__)
     
 @app.route('/')
 def home():
-    create_table()
-    add_module_count_trigger()
     return render_template('home.html')
 
 if __name__ == "__main__":
