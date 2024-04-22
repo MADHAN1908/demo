b9697d4b543858ae029f8880a4e7f14c03405a9f
madhan <162275786+MADHAN1908@users.noreply.github.com>
2024-04-22 14:53:53 +0530

    d

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from db import get_db 
 
-from db import *
 app = Flask(__name__)
-    
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    db = get_db()
+    data=[]
+    return render_template('home.html', data=data)  
 
 if __name__ == "__main__":
     app.run(debug=True)
